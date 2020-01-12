1fa15f51ac4e21ad5c90ae469225105a4406239c
Osuvaldo Ramos <osuvaldo@divvyhomes.com>
2020-01-11 20:42:08 -0800

    Add eslint-plugin-import and config
    Order and eparate types of imports (node builtin, external, etc.). Also separates imports starting with '@app/'.

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -3,12 +3,30 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'import'
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended" 
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
   ],
+  rules: {
+    'import/order': ['error', {
+      alphabetize: { order: 'asc' },
+      'newlines-between': 'always',
+      pathGroups: [
+        {
+          'pattern': '@app/**',
+          'group': 'external',
+          'position': 'after'
+        }
+      ],
+      pathGroupsExcludedImportTypes: ['builtin']
+    }],
+  }
 };
